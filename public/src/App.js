f9d536d8c1f327f3fabd0f9c65b23f45abb43526
drhemanm <heman.m@iafrica.solutions>
2025-08-25 12:40:59 +0400

    Create App.js

---
+++ public/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CRSXMLConverter from './components/CRSXMLConverter';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CRSXMLConverter />
+    </div>
+  );
+}
+
+export default App;
