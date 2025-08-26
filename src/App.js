9fb8570edcbf973f25696c7d69a2d3b0cbdb6e7a
drhemanm <heman.m@iafrica.solutions>
2025-08-26 14:15:29 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CRSXMLConverter from './components/CRSXMLConverter';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
+import Documentation from './pages/Documentation';
 import './index.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<CRSXMLConverter />} />
           <Route path="/privacy" element={<PrivacyPolicy />} />
           <Route path="/terms" element={<TermsOfService />} />
+          <Route path="/documentation" element={<Documentation />} />
         </Routes>
       </div>
     </Router>
