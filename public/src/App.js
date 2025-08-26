dc4d3f9a533c3c552c95e5f9b3e3a7a9ff9b61b2
drhemanm <heman.m@iafrica.solutions>
2025-08-26 15:29:01 +0400

    Update App.js

--- public/src/App.js
+++ public/src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import CRSXMLConverter from './components/CRSXMLConverter';
-import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <CRSXMLConverter />
-    </div>
-  );
-}
-
-export default App;
