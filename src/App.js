4ce22cbb639c12f7bc12fb3c4233991f9c2951f4
drhemanm <heman.m@iafrica.solutions>
2025-08-26 13:39:26 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CRSXMLConverter from './components/CRSXMLConverter';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <CRSXMLConverter />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<CRSXMLConverter />} />
+          <Route path="/privacy" element={<PrivacyPolicy />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
